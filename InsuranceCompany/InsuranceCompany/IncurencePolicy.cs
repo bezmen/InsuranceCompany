36922235659a4b30e5b575160a91a23985ff61f9
Alexey <lesha200111@gmail.com>
2019-05-25 10:44:41 +0300

    .

---
+++ InsuranceCompany/InsuranceCompany/IncurencePolicy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceCompany
+{
+    class IncurencePolicy
+    {
+        public int Number { get; set; }
+        public Employee Employee { get; set; }
+        public Category Category { get; set; }
+        public Client Client { get; set; }
+        public DateTime DateOfConclusion { get; set; }
+        public DateTime TermOfImprisonment { get; set; }
+        public decimal Sum { get; set; }
+        public decimal Cost { get; set; }
+        public int PaymentTerm { get; set; }
+       
+        
+        public IncurencePolicy(int number, Employee employee, Category category, Client client, DateTime dateOfConclusion, DateTime termOfImprisonment, decimal sum, decimal cost, int paymentTerm)
+        {
+            Number = number;
+            Employee = employee;
+            Category = category;
+            Client = client;
+        }
+    }
+}
